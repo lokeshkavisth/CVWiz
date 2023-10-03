96de760afad3954828ed6686906fb1ef34094e23
Lokesh Sharma <104786100+lokeshkavisth@users.noreply.github.com>
2023-10-03 18:10:58 +0530

    cleanup: remove unwanted libraries and their imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from 'react-router-dom'
 import { Auth0Provider } from '@auth0/auth0-react'
 
